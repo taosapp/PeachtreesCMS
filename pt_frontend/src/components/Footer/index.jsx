7277c7cf762c4eae0f0a21563715f2a69373da06
taosapp <taoland@outlook.com>
2026-07-27 16:35:18 +0800

    update footer

--- pt_frontend/src/components/Footer/index.jsx
+++ pt_frontend/src/components/Footer/index.jsx
@@ -3,19 +3,17 @@ import { sanitizeHtml } from '../../utils/sanitizeHtml'
 
 export default function Footer() {
   const { siteOptions } = useTheme()
-
-  const defaultFooter = `© ${new Date().getFullYear()} ${siteOptions.site_title || ''}`
-  const footerContent = siteOptions.footer_text || defaultFooter
+  const footerContent = siteOptions.footer_text;
 
   // 安全地过滤HTML
   const safeHtml = sanitizeHtml(footerContent)
 
   return (
     <footer className="footer">
-      <div
-        className="inner"
-        dangerouslySetInnerHTML={{ __html: safeHtml }}
-      />
+      <div className="inner">
+        <div className="footer-text" dangerouslySetInnerHTML={{ __html: safeHtml }} />
+        <p>Powered by <a href="https://github.com/taotaotao-studio/PeachtreesCMS" target="_blank">PeachtreesCMS</a> · <a href={`${import.meta.env.VITE_API_BASE_URL}rss.php`} target="_blank">RSS</a> </p>
+      </div>
     </footer>
   )
 }
